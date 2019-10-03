7845059561eee062f1749de09bd45c6696775d13
秦Daniel <daniel_chin@yahoo.com>
2019-10-03 00:54:12 -0400

    fix data structure; write down more design details; setting grand scheme in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,85 @@
-from threading import Thread, Lock, Condition
+from threading import Thread, Lock
 from socket import socket
 import sys
 import os
 from os import path
 import pickle
+from collections import namedtuple
+from subprocess import Popen
 
 LISTEN = 8
-DATABASE_FILENAME = ''
+DATABASE_PATH = 'database/'
+DATABASE_FILENAMES = ['0.pickle', '1.pickle']
+WHICH_FILENAME = 'which_file_is_valid.onebyte'
+
+Entry = namedtuple('Entry', [
+    'token', 
+    'amount', 
+    'currency_type', 
+    'time', 
+    'tags', 
+    'comment', 
+])
 
 def main():
     assert path.dirname(path.abspath(__file__)) == os.getcwd() ####
     port = int(sys.argv[1])
-    server = launchServer(port)
-    database = loadDatabase()
+    database = []
+    databaseLock = Lock()
+    with databaseLock:
+        serverThread = ServerThread(
+            database, databaseLock, port, RequestHandler
+        )
+        serverThread.start()
+        database.extend(loadDatabase()) # Just one node
+    serverThread.join()
 
-def launchServer(port):
-    server = socket()
-    server.bind(('localhost', port))
-    server.listen(LISTEN)
-    return server
+class ServerThread(Thread):
+    def __init__(self, database, databaseLock, port, RequestHandler):
+        super().__init__()
+        self.server = socket()
+        self.server.bind(('localhost', port))
+        self.server.listen(LISTEN)
+        self.RequestHandler = RequestHandler
+        self.shall_stop = False
+        self.port = port
+    
+    def stop():
+        self.shall_stop = True
+        waker = socket()
+        try:
+            waker.connect(('localhost', self.port))
+            waker.close()
+        except ConnectionRefusedError:
+            pass
+    
+    def run():
+        while not self.shall_stop:
+            s, addr = self.server.accept()
+            self.RequestHandler(s, addr, self.stop).start()
 
 def loadDatabase():
-    with open(DATABASE_FILENAME, 'rb')
+    home = os.getcwd()
+    os.chdir(DATABASE_PATH)
+    try:
+        with open(WHICH_FILENAME, 'rb') as f:
+            which = f.read()[0]
+        with open(DATABASE_FILENAMES[which], 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        print('Welcome to Where Dit Go! ')
+        database = [None, None, None]
+        for i in range(2):
+            with open(DATABASE_FILENAMES[i], 'wb+') as f:
+                pickle.dump(database, f)
+        with open(WHICH_FILENAME, 'wb+') as f:
+            f.write(b'\x00')
+        print('Initialized database. ')
+        gitInit = Popen(['git', 'init'])
+        gitInit.wait()
+        print('You are all set. ')
+        return database
+    finally:
+        os.chdir(home)
 
 main()
--- readme.md
+++ readme.md
@@ -4,14 +4,16 @@ Where did all my money go?
 
 Use this mobile app to record expenses.  
 
+It is under construction.  
+
 ## How to use
 Run `dit` in Termux, and it will open your browser to present the app.  
 
 ## features
-The money amount input interface supports +-*/.  
-Git backup and version track your database.  
-Robust interrupt-proof file saving strategy.  
-Export your data free of charge. (The main reason I wrote this for myself)  
+* The money amount input interface supports +-*/.  
+* Git backup and version track your database.  
+* Robust interrupt-proof file saving strategy.  
+* Export your data free of charge. (The main reason I wrote this for myself)  
 
 ## Implementation
 Declarative without optimization.  
@@ -19,4 +21,6 @@ For each update, request to get all entries from the databse.
 
 The JS frontend does all the computations. The Python backend is just a database.  
 
-It is under construction. 
+## Future works
+* If I can interrupt pickle.dump, then allow preemtive database writes.  
+* Does it make sense to git commit every time there is a DELETE request?  
