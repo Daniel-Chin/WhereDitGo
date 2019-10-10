fd389d1ca0031d980fe78eae8273a424969946a2
Daniel Chin <daniel_chin@yahoo.com>
2019-10-09 21:28:30 -0400

    backend: change returnTrue to lambda

--- backend/server.py
+++ backend/server.py
@@ -3,22 +3,19 @@ from request_handler import RequestHandler
 
 LISTEN = 3 * 1  # Axios better not be smart
 
-def returnTrue(*args, **kw):
-    return True
-
 class Server:
     def __init__(self, port):
         self.port = port
     
     def __enter__(self):
         self.sock = socket()
-        self.sock.bind(('localhost', port))
+        self.sock.bind(('localhost', self.port))
         self.sock.listen(LISTEN)
         return self, self.sock
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.sock.close()
     
-    def handleOne(self, database, validator = returnTrue):
+    def handleOne(self, database, validator = lambda *a, **k : True):
         s, addr = self.sock.accept()
         RequestHandler(s, addr, database).do(validator)
