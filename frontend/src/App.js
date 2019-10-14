08556fde371e3928459e649de67e7c447982b735
Daniel Chin <daniel_chin@yahoo.com>
2019-10-14 09:02:04 -0400

    experiment; router unmounts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 const App = (props) => {
   return (
+    <>
+    <Switch>
+      <Route exact path='/' component={C1} />
+      )}/>
+    </Switch>
+    <a href='a'>a</a>
+    <a href='/'>b</a>
+    </>
+  );
+}
+
+const C1 = () => {
+  const [s, setS] = useState(0);
+  return (
     <div>
-      BLANK
+      <button onClick={()=>{setS(4);}}>{s}</button>
     </div>
   );
 }
